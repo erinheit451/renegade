c54712c7555d3dbe9a63b027054ce6f2b50f8c72
erinheit451 <eringreen451@gmail.com>
2023-01-04 22:13:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,33 +10,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 conversation = []
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-   
-    #commenting out
-    return
-    global conversation
-
-    # Handle form submission
-    if request.method == "POST":
-        user_input = request.form["input"]
-        conversation.append({"user": user_input})
-
-        # Generate a response from the chatbot
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"{prompt}\n{user_input}",
-            temperature=0.9,
-            max_tokens=150,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6
-        )
-        chatbot_response = response.choices[0].text
-        conversation.append({"chatbot": chatbot_response})
-
-    return render_template("index.html", conversation=conversation)
-
 @app.route("/sms", methods=["POST"])
 def sms():
     # Get the message body from the request

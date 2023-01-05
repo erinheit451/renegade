be29e1d7db1c9219829dcc9ff4868152df456547
erinheit451 <eringreen451@gmail.com>
2023-01-04 23:49:07 -0600

    things please

---
+++ ai.py
@@ -0,0 +1,28 @@
+import os
+import openai
+import json
+from flask import Flask, request, Response, redirect, render_template, url_for
+
+with open("prompt.txt") as f:
+    prompt = f.read()
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route("/send_message", methods=["POST"])
+def send_message():
+    user_input = request.form["text"]
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{prompt}\n{user_input}",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6
+    )
+
+   # Create a TwiML response
+   twiml_response = f"<Response><Message>{response}</Message></Response>"
+
+   return Response(twiml_response, mimetype="text/xml")

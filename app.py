6f279c538c397544ff15062f645ade4833f2535f
erinheit451 <eringreen451@gmail.com>
2023-01-04 23:01:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import json
 from flask import Flask, request, Response, redirect, render_template, url_for
 
 with open("prompt.txt") as f:
@@ -8,54 +9,19 @@ with open("prompt.txt") as f:
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-conversation = []
-
-@app.route("/", methods=("GET", "POST"))
-def index():
-   
-    #commenting out
-    return
-    global conversation
-
-    # Handle form submission
-    if request.method == "POST":
-        user_input = request.form["input"]
-        conversation.append({"user": user_input})
-
-        # Generate a response from the chatbot
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"{prompt}\n{user_input}",
-            temperature=0.9,
-            max_tokens=150,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6
-        )
-        chatbot_response = response.choices[0].text
-        conversation.append({"chatbot": chatbot_response})
-
-    return render_template("index.html", conversation=conversation)
-
-@app.route("/sms", methods=["POST"])
-def sms():
-    # Get the message body from the request
-    body = request.form["Body"]
-
-    # Generate a response
+@app.route("/send_message", methods=["POST"])
+def send_message():
+    user_input = request.form["text"]
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"{prompt}{body}",
+        prompt=f"{prompt}\n{user_input}",
         temperature=0.9,
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6
     )
-    chatbot_response = response.choices[0].text
-
-
     # Create a TwiML response
-    twiml_response = f"<Response><Message>{chatbot_response}</Message></Response>"
+    twiml_response = f"<Response><Message>{response}</Message></Response>"
 
     return Response(twiml_response, mimetype="text/xml")

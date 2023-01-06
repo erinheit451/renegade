a41283a5dcb48ea41078c0f8cd989ce72ec1264d
erinheit451 <eringreen451@gmail.com>
2023-01-05 18:17:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,60 @@
 import os
 import openai
+from flask import Flask, request, Response, redirect, render_template, url_for
 
-# Read the prompt from a file
 with open("prompt.txt") as f:
     prompt = f.read()
 
-# Set the maximum conversation length
-MAX_CONVERSATION_LENGTH = 100
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Initialize the conversation history
 conversation = []
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+   
+    #commenting out
+    return
+    global conversation
+
+    # Handle form submission
+    if request.method == "POST":
+        user_input = request.form["input"]
+        conversation.append({"user": user_input})
+
+        # Generate a response from the chatbot
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"{prompt}\n{user_input}",
+            temperature=0.9,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6
+        )
+        chatbot_response = response.choices[0].text
+        conversation.append({"chatbot": chatbot_response})
+
+    return render_template("index.html", conversation=conversation)
 
 @app.route("/sms", methods=["POST"])
 def sms():
     # Get the message body from the request
     body = request.form["Body"]
 
-    # Check for the special command
-    if body.lower() == "reset chat log":
-        conversation.clear()  # Clear the conversation history
-        chatbot_response = "Chat log reset."  # Set the chatbot's response
-    else:
-        # Generate a response
-        chatbot_response = openai.Completion.create(engine="text-davinci-003", prompt=f"""{prompt}
-{'\n'.join([message for sender, message in conversation])}
-{body}""", temperature=0.9, max_tokens=150).text
-
-
-        # Store the conversation in the deque
-        conversation.append({"user": body})
-        conversation.append({"chatbot": chatbot_response})
-
-        # Save the conversation to a file
-        with open("chatlog.txt", "w") as f:
-            f.write("\n".join([f"{sender}: {message}" for sender, message in conversation]))
+    # Generate a response
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{prompt}{body}",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6
+    )
+    chatbot_response = response.choices[0].text
 
     # Create a TwiML response
     twiml_response = f"<Response><Message>{chatbot_response}</Message></Response>"
 
     return Response(twiml_response, mimetype="text/xml")
-
-@app.route("/startup", methods=["GET"])
-def startup():
-    # Send the "I'm awake" message
-    send_message("I'm awake!")
-
-    # Redirect the user to the chat log page
-    return redirect(url_for("chatlog"))
-
-@app.route("/chatlog", methods=["GET"])
-def chatlog():
-    # Read the chat log file
-    with open("chatlog.txt") as f:
-        chatlog = f.read()
-
-    # Render the chat log template
-    return render_template("chatlog.html", chatlog=chatlog)
-
-def send_message(message):
-    # Read the Twilio API credentials from the .env file
-    twilio_sid = os.getenv("TWILIO_SID")
-    twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-    from_number = os.getenv("TWILIO_FROM_NUMBER")
-
-    # Send the message using the Twilio API
-    client = Client(twilio_sid, twilio_auth_token)
-    client.messages.create(body=message, from_=from_number, to=to_number)

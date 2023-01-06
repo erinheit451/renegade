18a6787a1249697dffbab3d2c4f44a0f6d6422b3
erinheit451 <eringreen451@gmail.com>
2023-01-05 21:06:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import os
 import openai
 from flask import Flask, request, Response, redirect, render_template, url_for
 
-with open("prompt.txt") as f:
-    prompt = f.read()
-
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -12,19 +9,15 @@ conversation = []
 
 @app.route("/", methods=("GET", "POST"))
 def index():
-   
-
     global conversation
-
     # Handle form submission
     if request.method == "POST":
         user_input = request.form["input"]
         conversation.append({"user": user_input})
-
         # Generate a response from the chatbot
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=f"{prompt}\n{user_input}",
+            prompt=f"Harley helpline! How can I smash your problems into itty bitty pieces?\n{user_input}",
             temperature=0.9,
             max_tokens=150,
             top_p=1,
@@ -33,18 +26,16 @@ def index():
         )
         chatbot_response = response.choices[0].text
         conversation.append({"chatbot": chatbot_response})
-
     return render_template("index.html", conversation=conversation)
 
 @app.route("/sms", methods=["POST"])
 def sms():
     # Get the message body from the request
     body = request.form["Body"]
-
     # Generate a response
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"{prompt}{body}",
+        prompt=f"Harley helpline! How can I smash your problems into itty bitty pieces?\n{body}",
         temperature=0.9,
         max_tokens=150,
         top_p=1,
@@ -52,9 +43,6 @@ def sms():
         presence_penalty=0.6
     )
     chatbot_response = response.choices[0].text
-
-
     # Create a TwiML response
     twiml_response = f"<Response><Message>{chatbot_response}</Message></Response>"
-
     return Response(twiml_response, mimetype="text/xml")

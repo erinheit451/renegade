2a1d66d1309892ea5252b75b723b3f4d6ea084b9
erinheit451 <eringreen451@gmail.com>
2023-01-08 16:42:19 -0600

    trtwregsdfg

--- test.py
+++ test.py
@@ -2,21 +2,22 @@ import os
 import openai
 import telegram
 import requests
+import dotenv
+import chatlog
 from flask import Flask, request, Response, render_template
 from prompt import prompt
 from response import generate_chatbot_response
-from chatlog import log_conversation, load_conversation_log, prune_conversation_log
-from record import log_permanent_record, load_permanent_record
+from chatlog import log_conversation, load_conversation_log, prune_chatlog
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-api_token = os.getenv("TELEGRAM_API_TOKEN")
+dotenv.load_dotenv()
+bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
 
 app = Flask(__name__)
 
 conversation = load_conversation_log()
 log_conversation(conversation)
-
-bot = telegram.Bot(token=api_token)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
@@ -25,24 +26,27 @@ def index():
     if request.method == "POST":
         user_input = request.form["input"]
         conversation.append({"user": user_input})
-        # Generate a response from the chatbot
-        chatlog = prune_conversation_log(conversation)
+        # Prune the conversation to ensure it doesn't exceed the maximum number of tokens
+        conversation = prune_chatlog(conversation, 300)
+        # Generate a chatlog by pruning the conversation
+        chatlog = prune_chatlog(conversation)
         chatbot_response = generate_chatbot_response(prompt, user_input, chatlog)
         conversation.append({"chatbot": chatbot_response})
-        log_permanent_record(conversation)
     return render_template("index.html", conversation=conversation)
+
 
 
 @app.route("/sms", methods=["POST"])
 def sms():
+    global conversation
     # Get the message body from the request
     body = request.form["Body"]
+    # Prune the conversation to ensure it doesn't exceed the maximum number of tokens
+    conversation = prune_chatlog(conversation, 300)
     # Generate a response
-    chatlog = prune_conversation_log(conversation)
+    chatlog = prune_chatlog(conversation)
     chatbot_response = generate_chatbot_response(prompt, body, chatlog)
     conversation.append({"chatbot": chatbot_response})
-    log_permanent_record(conversation)
     # Create a TwiML response
     twiml_response = f"<Response><Message>{chatbot_response}</Message></Response>"
     return Response(twiml_response, mimetype="text/xml")
-

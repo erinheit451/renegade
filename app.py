2b52aceb22ed6eef345e2443861585719ebb9724
erinheit451 <eringreen451@gmail.com>
2023-01-16 00:17:51 -0600

    bot test

--- app.py
+++ app.py
@@ -1,49 +1,26 @@
 import os
-import openai
 import telegram
-import logging
-from flask import Flask, request
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
-from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 bot_token = os.environ["BOT_TOKEN"]
 bot = telegram.Bot(token=bot_token)
 
-def handle_text(update: Update, context: CallbackContext):
-    message_text = update.message.text
-    chat_id = update.message.chat.id
-    response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=message_text,
-            max_tokens=250,
-            temperature=0.7
-    )
-    bot.send_message(chat_id=chat_id, text=response["choices"][0]["text"])
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I am a bot.")
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    update = request.get_json()
-    context = CallbackContext()
-    if 'message' in update:
-        handle_text(update, context)
-    return "OK"
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I am a simple bot. Send me a message and I will respond.")
+
+def message(update, context):
+    message_text = update.message.text
+    context.bot.send_message(chat_id=update.effective_chat.id, text="You said: " + message_text)
 
 updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
-dispatcher.add_handler(MessageHandler(Filters.text, handle_text))
 
-def start(update: Update, context: CallbackContext):
-    chat_id = update.message.chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hi!")
-dispatcher.add_handler(CommandHandler("start", start))
-
-# Logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-updater.start_polling()
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(MessageHandler(Filters.text, message))
 
 if __name__ == '__main__':
-    app.run()
+    updater.start_polling()
